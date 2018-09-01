3cadc4db8bd53057cb08709c9ef760ab157f25f7
“Taiyo <taitai9847@gmail.com>
2018-09-01 12:11:37 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
+
   def destroy
-   log_out
-   redirect_to root_url
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
